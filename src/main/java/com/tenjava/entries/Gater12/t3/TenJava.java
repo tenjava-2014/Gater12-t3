0cab495be3fcce41389c48e592d0b579031699e1
Bob Paulowaski <mcraftworldmc@gmail.com>
2014-07-12 07:27:27 -0700

    Initial Commit

--- src/main/java/com/tenjava/entries/Gater12/t3/TenJava.java
+++ src/main/java/com/tenjava/entries/Gater12/t3/TenJava.java
@@ -2,4 +2,10 @@ package com.tenjava.entries.Gater12.t3;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+	
+	@Override
+	public void onEnable(){
+		
+	}
+}
